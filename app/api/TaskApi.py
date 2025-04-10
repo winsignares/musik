450ac1f7d259f5e5d044f571405a051480e37a62
Jose Polo <josedanielpolo04@hotmail.com>
2025-04-10 15:38:05 -0500

    apis v3

---
+++ app/api/TaskApi.py
@@ -0,0 +1,34 @@
+from flask import Flask, Blueprint, request, redirect, render_template, jsonify
+from config.db import db, app, ma
+
+from models.TaskModel import Task, TaskSchema
+
+task_schema = TaskSchema()
+tasks_schema = TaskSchema(many=True)
+
+#endpoints
+
+ruta_task = Blueprint('ruta_task', __name__)
+@ruta_task.route ('/task', methods=['GET'])
+def alltask():
+    resultAll = Task.query.all() #select * from users
+    resp = tasks_schema(resultAll)
+    return jsonify(resp)
+
+@ruta_task.route('/saveTask', methods=['POST'])
+def saveTask():
+    nametask = request.json['nametask']
+    id_user_fk = request.json['id_user_fk']
+    id_category_fk = request.json['id_category_fk']
+    newtask = Task(nametask, id_user_fk, id_category_fk)
+    db.session.add(newtask)
+    db.session.commit()
+    return 'Datos guardados con éxito'
+
+@ruta_task.route('/deleteTask', methods=['DELETE'])
+def deleteTask():
+    id = request.json['id']
+    task = Task.query.get(id)
+    db.session.delete(task)
+    db.session.commit()
+    return 'Datos eliminados con éxito'
--- app/models/TaskModel.py
+++ app/models/TaskModel.py
@@ -1,15 +1,15 @@
 from config.db import app, db, ma
 
-class Taks(db.Model):
-    __tablename__ = 'tbl_taks'
+class Task(db.Model):
+    __tablename__ = 'tbl_task'
     
     id = db.Column(db.Integer, primary_key = True)
-    nametaks = db.Column(db.String(100))
+    nametask = db.Column(db.String(100))
     id_user_fk = db.Column(db.Integer, db.ForeignKey('tbl_users.id'))
     id_category_fk = db.Column(db.Integer, db.ForeignKey('tbl_category.id'))
     
-    def __init__(self, nametaks, id_user_fk, id_category_fk):
-        self.nametaks = nametaks
+    def __init__(self, nametask, id_user_fk, id_category_fk):
+        self.nametask = nametask
         self.id_user_fk = id_user_fk
         self.id_category_fk = id_category_fk
         
@@ -17,6 +17,6 @@ class Taks(db.Model):
 with app.app_context():
     db.create_all()
     
-class TaksSchema(ma.Schema):
+class TaskSchema(ma.Schema):
     class Meta:
         fields = ('id', 'nametaks', 'id_user_fk', 'id_category_fk')
