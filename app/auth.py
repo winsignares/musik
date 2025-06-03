a2a50c53289f757f2ea7b05f26b6511f6194688a
Juan <jtuiran4@gmail.com>
2025-06-03 14:27:35 -0500

    Casi

--- app/auth.py
+++ app/auth.py
@@ -4,8 +4,9 @@ from flask import session, redirect, url_for, flash
 def admin_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if 'user_id' not in session or session.get('rol') != 'admin':
+        if 'user_id' not in session or session.get('user_role') != 'admin':
             flash("No tienes permiso para acceder a esta página", "error")
             return redirect(url_for('principal'))
         return f(*args, **kwargs)
     return decorated_function
+
